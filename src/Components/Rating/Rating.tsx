f49b314213f6fef59d43cae3cb913f7be3deb765
Максимов Александр Александрович <aa.maksimov@skbkontur.ru>
2023-10-15 10:47:51 +0500

    fix

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -3,14 +3,12 @@ import { Star } from './Star.tsx';
 type RatingProps = {
   onClick: (value: number) => void;
 };
-export const Rating = ({ onClick }: RatingProps) => {
-  return (
-    <div className="rating">
-      <div className="rating__stars">
-        {[...Array(10)].map((_, i) => (
-          <Star key={10 - i} value={10 - i} onClick={() => onClick(10 - i)} />
-        ))}
-      </div>
+export const Rating = ({ onClick }: RatingProps) => (
+  <div className="rating">
+    <div className="rating__stars">
+      {Array.from(Array(10).keys()).map((i) => (
+        <Star key={10 - i} value={10 - i} onClick={() => onClick(10 - i)} />
+      ))}
     </div>
-  );
-};
+  </div>
+);
--- src/Components/Rating/Star.tsx
+++ src/Components/Rating/Star.tsx
@@ -3,20 +3,18 @@ type StarProps = {
   onClick: () => void;
 };
 
-export const Star = ({ value, onClick }: StarProps) => {
-  return (
-    <>
-      <input
-        className="rating__input"
-        id={`star-${value}`}
-        type="radio"
-        name="rating"
-        value={value}
-        onClick={onClick}
-      />
-      <label className="rating__label" htmlFor={`star-${value}`}>
-        Rating {value}
-      </label>
-    </>
-  );
-};
+export const Star = ({ value, onClick }: StarProps) => (
+  <>
+    <input
+      className="rating__input"
+      id={`star-${value}`}
+      type="radio"
+      name="rating"
+      value={value}
+      onClick={onClick}
+    />
+    <label className="rating__label" htmlFor={`star-${value}`}>
+      Rating {value}
+    </label>
+  </>
+);
