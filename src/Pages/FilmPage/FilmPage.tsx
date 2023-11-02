bf29e1d994acc6df75212bc662a0a3aa45fbbcd0
Максимов Александр Александрович <aa.maksimov@skbkontur.ru>
2023-11-02 20:45:17 +0500

    Done

--- src/Pages/FilmPage/FilmPage.tsx
+++ src/Pages/FilmPage/FilmPage.tsx
@@ -1,123 +1,102 @@
 import { Footer } from '../../Components/Footer/Footer.tsx';
 import { Header } from '../../Components/Header/Header.tsx';
+import { Tabs } from '../../Components/Tabs/Tabs.tsx';
+import { Tab } from '../../Components/Tabs/Tab.tsx';
+import { OverviewTab } from './Tabs/OverviewTab.tsx';
+import { DetailsTab } from './Tabs/DetailsTab.tsx';
+import { ReviewsTab } from './Tabs/ReviewsTab.tsx';
+import { Link, useParams } from 'react-router-dom';
+import { useFilm } from '../../Mocks/useFilm.ts';
+import { appRoutes } from '../../appRoutes.ts';
+import { FilmCardList } from '../../Components/FilmCardList/FilmCardList.tsx';
+import { useFilms } from '../../Mocks/useFilms.tsx';
 
-export const FilmPage = () => (
-  <div>
-    <section className="film-card film-card--full">
-      <div className="film-card__hero">
-        <div className="film-card__bg">
-          <img
-            src="img/bg-the-grand-budapest-hotel.jpg"
-            alt="The Grand Budapest Hotel"
-          />
-        </div>
+export const FilmPage = () => {
+  const { id } = useParams();
+  const film = useFilm({ id: Number(id) });
+  const films = useFilms();
 
-        <h1 className="visually-hidden">WTW</h1>
+  return (
+    <div>
+      <section className="film-card film-card--full">
+        <div className="film-card__hero">
+          <div className="film-card__bg">
+            <img src={film.imgSrc} alt={film.name} />
+          </div>
 
-        <Header />
+          <h1 className="visually-hidden">WTW</h1>
 
-        <div className="film-card__wrap">
-          <div className="film-card__desc">
-            <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-            <p className="film-card__meta">
-              <span className="film-card__genre">Drama</span>
-              <span className="film-card__year">2014</span>
-            </p>
+          <Header />
 
-            <div className="film-card__buttons">
-              <button className="btn btn--play film-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list film-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-                <span className="film-card__count">9</span>
-              </button>
-              <a href="add-review.html" className="btn film-card__button">
-                Add review
-              </a>
+          <div className="film-card__wrap">
+            <div className="film-card__desc">
+              <h2 className="film-card__title">{film.name}</h2>
+              <p className="film-card__meta">
+                <span className="film-card__genre">Drama</span>
+                <span className="film-card__year">2014</span>
+              </p>
+
+              <div className="film-card__buttons">
+                <button
+                  className="btn btn--play film-card__button"
+                  type="button"
+                >
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button
+                  className="btn btn--list film-card__button"
+                  type="button"
+                >
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                  <span className="film-card__count">9</span>
+                </button>
+                <Link
+                  to={appRoutes.AddReview(id || '')}
+                  className="btn film-card__button"
+                >
+                  Add review
+                </Link>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className="film-card__wrap film-card__translate-top">
+          <div className="film-card__info">
+            <div className="film-card__poster film-card__poster--big">
+              <img src={film.imgSrc} alt={film.name} width="218" height="327" />
+            </div>
+
+            <div className="film-card__desc">
+              <Tabs>
+                <Tab name="Overview">
+                  <OverviewTab />
+                </Tab>
+                <Tab name="Details">
+                  <DetailsTab />
+                </Tab>
+                <Tab name="Reviews">
+                  <ReviewsTab />
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+          <FilmCardList films={films} />
+        </section>
+        <Footer />
       </div>
-
-      <div className="film-card__wrap film-card__translate-top">
-        <div className="film-card__info">
-          <div className="film-card__poster film-card__poster--big">
-            <img
-              src="img/the-grand-budapest-hotel-poster.jpg"
-              alt="The Grand Budapest Hotel poster"
-              width="218"
-              height="327"
-            />
-          </div>
-
-          <div className="film-card__desc">
-            <nav className="film-nav film-card__nav">
-              <ul className="film-nav__list">
-                <li className="film-nav__item film-nav__item--active">
-                  <a href="#" className="film-nav__link">
-                    Overview
-                  </a>
-                </li>
-                <li className="film-nav__item">
-                  <a href="#" className="film-nav__link">
-                    Details
-                  </a>
-                </li>
-                <li className="film-nav__item">
-                  <a href="#" className="film-nav__link">
-                    Reviews
-                  </a>
-                </li>
-              </ul>
-            </nav>
-
-            <div className="film-rating">
-              <div className="film-rating__score">8,9</div>
-              <p className="film-rating__meta">
-                <span className="film-rating__level">Very good</span>
-                <span className="film-rating__count">240 ratings</span>
-              </p>
-            </div>
-
-            <div className="film-card__text">
-              <p>
-                In the 1930s, the Grand Budapest Hotel is a popular European ski
-                resort, presided over by concierge Gustave H. (Ralph Fiennes).
-                Zero, a junior lobby boy, becomes Gustave&apos;s friend and
-                protege.
-              </p>
-
-              <p>
-                Gustave prides himself on providing first-class service to the
-                hotel&apos;s guests, including satisfying the sexual needs of
-                the many elderly women who stay there. When one of
-                Gustave&apos;s lovers dies mysteriously, Gustave finds himself
-                the recipient of a priceless painting and the chief suspect in
-                her murder.
-              </p>
-
-              <p className="film-card__director">
-                <strong>Director: Wes Anderson</strong>
-              </p>
-
-              <p className="film-card__starring">
-                <strong>
-                  Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe
-                  and other
-                </strong>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <Footer />
-  </div>
-);
+    </div>
+  );
+};
