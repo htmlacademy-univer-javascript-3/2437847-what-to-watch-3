ac85fef414e66cf674f0f2c289580fcf75e53c8b
Максимов Александр Александрович <aa.maksimov@skbkontur.ru>
2023-11-29 20:41:45 +0500

    add click on progressBar

--- src/Pages/PlayerPage/PlayerPage.tsx
+++ src/Pages/PlayerPage/PlayerPage.tsx
@@ -1,22 +1,23 @@
 import { useFilm } from '../../Hooks/films.ts';
 import { usePathId } from '../../Hooks/usePathId.ts';
 import { convertTimeToPlayerFormat } from '../../Helpers/Time.ts';
-import { useEffect, useRef, useState } from 'react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { appRoutes } from '../../appRoutes.ts';
+import './progressBar.css';
 
 export const PlayerPage = () => {
   const id = usePathId();
   const { data: film } = useFilm(id);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [progressBar, setProgressBar] = useState(0);
+  const [progress, setProgress] = useState(0);
   const [playerTime, setPlayerTime] = useState(0);
   const videoRef = useRef<HTMLVideoElement>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isPlaying) {
-      videoRef.current?.play();
+      videoRef.current?.play().then();
     } else {
       videoRef.current?.pause();
     }
@@ -30,7 +31,7 @@ export const PlayerPage = () => {
     const video = videoRef.current;
 
     if (video) {
-      video.requestFullscreen();
+      video.requestFullscreen().then();
     }
   };
 
@@ -43,12 +44,34 @@ export const PlayerPage = () => {
 
       const progress = (currentTime / duration) * 100;
       setPlayerTime(Math.floor(duration - currentTime));
-      setProgressBar(progress);
+      setProgress(progress);
+    }
+  };
+
+  const progressBarRef = useRef<HTMLProgressElement>(null);
+
+  const handleProgressBarClick = (event: MouseEvent<HTMLProgressElement>) => {
+    if (videoRef.current && progressBarRef.current) {
+      const progressBar = progressBarRef.current;
+      const rect = progressBar.getBoundingClientRect();
+      const offsetX = event.clientX - rect.left;
+      const progressBarWidth = progressBar.offsetWidth;
+
+      const progress = (offsetX / progressBarWidth) * 100;
+      setProgress(progress);
+
+      const video = videoRef.current;
+      if (video) {
+        const duration = video.duration;
+        const currentTime = (progress * duration) / 100;
+        video.currentTime = currentTime;
+        setPlayerTime(Math.floor(duration - currentTime));
+      }
     }
   };
 
   return (
-    <div className="player">
+    <div className="player" style={{ userSelect: 'none' }}>
       <video
         src={film?.videoLink}
         className="player__video"
@@ -70,13 +93,12 @@ export const PlayerPage = () => {
           <div className="player__time">
             <progress
               className="player__progress"
-              value={progressBar}
+              value={progress}
               max="100"
+              ref={progressBarRef}
+              onClick={handleProgressBarClick}
             />
-            <div
-              className="player__toggler"
-              style={{ left: `${progressBar}%` }}
-            >
+            <div className="player__toggler" style={{ left: `${progress}%` }}>
               Toggler
             </div>
           </div>
